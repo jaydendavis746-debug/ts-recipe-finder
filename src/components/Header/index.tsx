d39e4a047370c54287b2e7a3d3e27a1b572aa23a
jaydendavis746-debug <jaydendavis746@gmail.com>
2026-09-17 10:38:29 +0100

    Basic homepage and nav bar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <>
+    <header>
+        <nav>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/recipes'>All recipes</NavLink>
+        </nav>
+    </header>
+    <Outlet/>
+    </>
+  )
+}
