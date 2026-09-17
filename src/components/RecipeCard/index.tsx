220b9eb07dcf35199f9b1d53a66e949c8ee3a1ed
AshSensei13 <ashley.bradley@hotmail.co.uk>
2026-09-17 15:15:30 +0100

    Update: added roles for html tags in RecipeCard, Add: RecipeCard tests

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -9,9 +9,9 @@ export default function RecipeCard ({recipe}: CardProps) {
             </div>
             <div>
                 <h2 className='recipe-title' >{recipe.title}</h2>
-                <em>{recipe.cuisine}, {recipe.course}</em>
-                <div dangerouslySetInnerHTML={{ __html: recipe.ingredients }}></div>
-                <div dangerouslySetInnerHTML={{ __html: recipe.directions }}></div>
+                <em role='figure'>{recipe.cuisine}, {recipe.course}</em>
+                <div role='figure2' dangerouslySetInnerHTML={{ __html: recipe.ingredients }}></div>
+                <div role='figure3' dangerouslySetInnerHTML={{ __html: recipe.directions }}></div>
             </div>
         </div>
     )
